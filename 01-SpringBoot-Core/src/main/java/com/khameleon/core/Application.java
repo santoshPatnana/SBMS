668ac24649c86700660e5a7ae18a37d808e606fe
santoshPatnana <ptnnsantosh@gmail.com>
2021-12-25 19:35:45 +0530

    1-Branch Added comment in main method

--- 01-SpringBoot-Core/src/main/java/com/khameleon/core/Application.java
+++ 01-SpringBoot-Core/src/main/java/com/khameleon/core/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	//Added comments in 1-Branch
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
