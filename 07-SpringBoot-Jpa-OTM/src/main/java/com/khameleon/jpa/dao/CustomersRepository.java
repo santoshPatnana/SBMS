42362fc67ff635cbfed70b11f44701bb79067fad
Santosh <Santosh@Santosh-PC>
2022-01-21 09:07:12 +0530

    fixed Query issue

--- 07-SpringBoot-Jpa-OTM/src/main/java/com/khameleon/jpa/dao/CustomersRepository.java
+++ 07-SpringBoot-Jpa-OTM/src/main/java/com/khameleon/jpa/dao/CustomersRepository.java
@@ -11,8 +11,8 @@ import com.khameleon.jpa.entity.Customers;
 @Repository
 public interface CustomersRepository extends JpaRepository<Customers, Integer> {
 	
-	@Query(value = "select c.CUST_NAME, o.ORDER_ID, o.ORDER_DATE from Customers c inner join  Orders o on c.CUST_ID = o.CUSTID_FK where c.CUST_ID = ? and o.IS_CASH_ON_DELIVERY = ?", nativeQuery=true)
-	//@Query(value = "select c.custName, o.orderId, o.orderDate from Customers c INNER JOIN  Orders o WHERE c.custId = ?1 and o.isCashOnDelivery = ?2")
+	//@Query(value = "select c.CUST_NAME, o.ORDER_ID, o.ORDER_DATE from Customers c inner join  Orders o on c.CUST_ID = o.CUSTID_FK where c.CUST_ID = ? and o.IS_CASH_ON_DELIVERY = ?", nativeQuery=true)	
+	@Query(value = "select c.custName, o.orderId, o.orderDate from Customers c INNER JOIN  c.orders o WHERE c.custId = ?1 and o.isCashOnDelivery = ?2")
 	List<Object[]> fetchCustomerWithCashOnDeliveryOrders(Integer custId,Boolean isCashOnDelivery);
 
 }
